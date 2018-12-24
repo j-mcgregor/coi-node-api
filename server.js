4c7193fe7ff378265d625117bea4d1dd12c09cbc
patchyj <jackjwmcgregor@gmail.com>
2018-12-24 11:21:08 +0000

    added headers

--- server.js
+++ server.js
@@ -15,7 +15,16 @@ const app = express();
 
 // Enable CORS
 app.use(cors());
-app.options("*", cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
