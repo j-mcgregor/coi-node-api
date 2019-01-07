7a33a54146fdc4bda01f75f9e026bb3785e53354
patchyj <jackjwmcgregor@gmail.com>
2019-01-07 18:58:31 +0000

    added an update for posts

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,31 +1,31 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const mongoose = require("mongoose");
-const passport = require("passport");
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 // Models
-const Project = require("../../models/Project");
+const Project = require('../../models/Project');
 
 // validation
-const validateProjectInput = require("../../validation/project");
+const validateProjectInput = require('../../validation/project');
 
 // @route    GET api/projectss/test
 // @desc     Tests Projects Route
 // @access   Public
 
-router.get("/test", (req, res) => res.json({ msg: "Projects works" }));
+router.get('/test', (req, res) => res.json({ msg: 'Projects works' }));
 
 // @route    GET api/projectss
 // @desc     Get all Projects
 // @access   Public
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   Project.find()
-    .populate("user")
+    .populate('user')
     .then(projectss => res.json(projectss))
     .catch(err =>
-      res.status(404).json({ noprojectssfound: "No Projects found" })
+      res.status(404).json({ noprojectssfound: 'No Projects found' })
     );
 });
 
@@ -33,12 +33,12 @@ router.get("/", (req, res) => {
 // @desc     Get single project
 // @access   Public
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   Project.findById(req.params.id)
-    .populate("user", ["_id", "firstName", "lastName"])
+    .populate('user', ['_id', 'firstName', 'lastName'])
     .then(project => res.json(project))
     .catch(err =>
-      res.status(404).json({ noprojectsfound: "No Project found with that ID" })
+      res.status(404).json({ noprojectsfound: 'No Project found with that ID' })
     );
 });
 
@@ -47,8 +47,8 @@ router.get("/:id", (req, res) => {
 // @access   Private
 
 router.post(
-  "/",
-  passport.authenticate("jwt", { session: false }),
+  '/',
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     const { errors, isValid } = validateProjectInput(req.body);
 
@@ -72,20 +72,38 @@ router.post(
   }
 );
 
+// @route    UPDATE api/projects/:id
+// @desc     Update Project
+// @access   Private
+
+router.put(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+    Project.findByIdAndUpdate(req.params.id, req.body)
+      .then(project => res.status(200).json(project))
+      .catch(err => {
+        errors.projectnotfound = 'Project Not Found';
+        return res.status(400).json(errors);
+      });
+  }
+);
+
 // @route    DELETE api/projectss/:id
 // @desc     Delete Projects
 // @access   Private
 
 router.delete(
-  "/:id",
-  passport.authenticate("jwt", { session: false }),
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Project.findById(req.params.id)
       .then(project => {
         // Check for project owner
         if (!req.user.admin) {
           return res.status(401).json({
-            unauthorised: "User not Authorized",
+            unauthorised: 'User not Authorized',
             project: project,
             user: req.user
           });
@@ -95,7 +113,7 @@ router.delete(
         project.remove();
       })
       .catch(err =>
-        res.status(404).json({ projectsnotfound: "No Project Found" })
+        res.status(404).json({ projectsnotfound: 'No Project Found' })
       );
   }
 );
